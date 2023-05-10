f7192be028c7427430921cc4f8277c28af4a174b
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-05-10 23:33:39 +0000

    criando os arquivos da base de dados

---
+++ src/main/java/models/Autor.java
@@ -0,0 +1,11 @@
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "autores")
+
+public class Autor{
+    
+}
---
+++ src/main/java/models/Genero.java
@@ -0,0 +1,9 @@
+import javax.persistence.Table;
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "generos")
+public class Genero{
+    
+}
